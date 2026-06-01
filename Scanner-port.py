bc32d2c52e04f3ccb667b78354622fbb183f621a
danylepere <danylepere21@gmail.com>
2026-06-01 08:16:27 +0100

    Update Scanner-port.py

--- Scanner-port.py
+++ Scanner-port.py
@@ -1,16 +1,17 @@
 import socket
-print("=== Mini scanner de Ports ===")
-target= input(" IP ou domaine à scanner : ")
-port = int(input(" Port à tester, par exemple : 22, 80 ,43 : "))
-print(f"Scan de (target) port (port)...")
-s = socket.socket(socket.AF_INET, SOCK_STREAM)
+print("=== Mini Scanner de Ports === ")
+target = input(" IP ou domaine à scanner :")
+port = int(input(" Port à tester, par exemple: 22, 80, 43 :")
+print(f"Scan de {target} port {port}...")
+           s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.settimeout(1)
 try
-    if s.connect_ex(target,port) == 0 :
-        print(f"Resultat : Port (port) est OUVERT 🔓")
-    else :
-        print(f"Resultat : Port (port) est FERMÉ 🔒")
+  if s.connext_ex(target,port) == 0
+    print(f"Resultat : Port{port} est OUVERT 🔓 ")
+  else :
+    print(f" Resultat : Port{port} est FERMÉ 🔒 ")
 except :
-    print(" Erreur : IP ou hôte injoignable ")
+  print("" Erreur : IP ou hôte injoignable ")
 s.close()
-print(" Scan terminé")
+print(" Scan terminé ")
+  
